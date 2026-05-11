d173a3becc072e76b706b1c731b2062b5b0f1970
LwinHtooPaing <lwinhtoopaing846@gmail.com>
2026-05-11 12:50:13 +0630

    Delete musicPlayer/todoList directory

--- musicPlayer/todoList/src/selector.js
+++
@@ -1,8 +0,0 @@
-export const inputTask = document.getElementById("inputTask");
-export const addBtn = document.getElementById("addBtn");
-export const todoTemplate = document.getElementById("todoTemplate");
-export const listGroup = document.getElementById("listGroup");
-export const totalTask = document.getElementById("totalTask");
-export const doneTotalTask = document.getElementById("doneTotalTask");
-export const noList = document.getElementById("noList");
-export const deleteAllBtn = document.getElementById("deleteAllBtn");
